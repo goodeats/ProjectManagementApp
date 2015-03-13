834590db23e20aed1ceb532d1ae7e1e558813c42
Annie <anne.bourgeois1@gmail.com>
2015-03-13 15:52:37 -0400

    Created templates for development

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,45 @@
 /*global Backbone:true */
 'use strict';
 
+// These are our front-end routes
+// http://localhost:9000/home (sign-in/sign-up)
+// http://localhost:9000/users
+// http://localhost:9000/users/id
+// http://localhost:9000/projects
+// http://localhost:9000/projects/id
+// http://localhost:9000projects/id/tasks/id
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'home',
+    'home': 'home'  //http://localhost:9000/#/home
+  },
+
+  home: function(){
+    $('#container').empty();
+  }
+
+});
+
+var mainRouter = new Router();
+Backbone.history.start();
+
+// (document).ajaxStart(function(e){
+//   trace(e, "starting an ajax request");
+//   $('section#ajax-preloader').fadeIn();
+//   $('section#container').fadeOut();
+// });
+
+// $(document).ajaxComplete(function(event, xhr, settings) {
+//   /* executes whenever an AJAX request completes */
+//   $('section#ajax-preloader').fadeOut();
+//   $('section#container').fadeIn();
+// });
+
 $(document).ready(function(){
   console.log('\'allo!');
   $( "div#form-slide" ).hide();
-  $( "#sign-up" ).click(function () {
+  $( 'button#sign-up' ).click(function () {
     if ( $( "div#form-slide" ).is( ":hidden" ) ) {
       $( "div#form-slide" ).slideDown( "slow" );
     } else {
