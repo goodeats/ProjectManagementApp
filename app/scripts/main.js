263b845f9fa023bd8b6f79a8ffec49b53194e725
pat <patrick.h.needham@gmail.com>
2015-03-17 10:43:20 -0400

    can access a single user now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,30 +24,78 @@ var Router = Backbone.Router.extend({
   routes: {
     'home': 'home',  //http://localhost:9000/#/home,
     'users': 'users', // http://localhost:9000/users
-    'users/:id': 'user'  //http://localhost:9000/#/users/1
+    'users/:id': 'user',  //http://localhost:9000/#/users/1
+    'projects': 'projects'
   },
 
   home: function(){
     $('#container').empty();
   },
   users: function(){
-    App.users();
-  }
+    App.usersRouter();
+  },
+  user: function(id){
+    App.userRouter(id);
+  },
+  projects: function(){
+    App.projectsRouter();
+  },
 
 });
 
 App.usersRouter = function(){
-  trace('hello from the user backbone!');
+  trace('hello from the users backbone!');
   $('#container').empty();
   $.ajax({
-    url: App.url + '/users', // add id after this works
+    url: App.url + '/users',
     type: 'GET'
   }).done(function(response){
     var template = Handlebars.compile($('#usersTemplate').html());
     $('#container').html(template({
       users: response.users
     }));
-    trace('in the users ajax');
+    $('.user').hide();
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  }).always(function(response){
+    trace(response);
+  });
+};
+
+App.userRouter = function(id){
+  trace('hello from the user backbone!');
+  $('#container').empty();
+  var locate = window.location.hash;
+  var point = locate.lastIndexOf('/');
+  var userId = parseInt(locate.substring(point+1, locate.length));
+  $.ajax({
+    url: App.url + '/users/' + userId,
+    type: 'GET'
+  }).done(function(response){
+    var template = Handlebars.compile($('#userTemplate').html());
+    $('#container').html(template({
+      user: response.user
+    }));
+    // $('.user').hide();
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    trace(jqXHR, textStatus, errorThrown);
+  }).always(function(response){
+    trace(response);
+  });
+};
+
+App.projectsRouter = function(){
+  trace('hello from the projects backbone!');
+  $('#container').empty();
+  $.ajax({
+    url: App.url + '/projects',
+    type: 'GET'
+  }).done(function(response){
+    var template = Handlebars.compile($('#projectsTemplate').html());
+    $('#container').html(template({
+      projects: response.projects
+    }));
+    $('.projects').hide();
   }).fail(function(jqXHR, textStatus, errorThrown){
     trace(jqXHR, textStatus, errorThrown);
   }).always(function(response){
@@ -58,7 +106,7 @@ App.usersRouter = function(){
 var router = new Router();
 Backbone.history.start();
 
-// (document).ajaxStart(function(e){
+// $(document).ajaxStart(function(e){
 //   trace(e, 'starting an ajax request');
 //   $('section#ajax-preloader').fadeIn();
 //   $('section#container').fadeOut();
@@ -83,5 +131,21 @@ $(document).ready(function(){
   });
 
   App.usersRouter();
-  $('div.users').hide();
+  $('#userlink').click(function() {
+    $('.jumbotron').hide();
+    $('.user').show();
+  });
+
+  App.userRouter();
+  $('#userlink').click(function() {
+    $('.jumbotron').hide();
+    $('.user').show();
+  });
+
+  App.projectsRouter();
+  $('#projectslink').click(function() {
+    $('.jumbotron').hide();
+    $('.projects').show();
+  });
+
 });
--- app/scripts/projects.js
+++ app/scripts/projects.js
@@ -3,11 +3,11 @@
 /*global Backbone:true */
 'use strict';
 
-var trace = function(){
-  for(var i = 0; i < arguments.length; i++){
-    console.log(arguments[i]);
-  }
-};
+// var trace = function(){
+//   for(var i = 0; i < arguments.length; i++){
+//     console.log(arguments[i]);
+//   }
+// };
 
 // var ProjectRouter = Backbone.Router.extend({
 //   routes: {
@@ -26,56 +26,53 @@ var trace = function(){
 //   }
 // });
 
-var renderProject = function(projects){
-  trace('render the project');
-  var html = '';
-  for(var i = 0; i < projects.length; i++){
-    html += '<div class="projects" id="project-' + projects[i].id + '">';
-    html += '<article>';
-    html += '<h2>' + projects[i].name + '</h2>';
-    html += '<p>Description: ' + projects[i].description + '</p>';
-    html += '<p>Due Date: ' + projects[i].due_date + '</p>';
-    html += '<p>Private Project: ' + projects[i].privacy + '</p>';
-    html += '</article></div>';
+// var renderProject = function(projects){
+//   trace('render the project');
+//   var html = '';
+//   for(var i = 0; i < projects.length; i++){
+//     html += '<div class="projects" id="project-' + projects[i].id + '">';
+//     html += '<article>';
+//     html += '<h2>' + projects[i].name + '</h2>';
+//     html += '<p>Description: ' + projects[i].description + '</p>';
+//     html += '<p>Due Date: ' + projects[i].due_date + '</p>';
+//     html += '<p>Private Project: ' + projects[i].privacy + '</p>';
+//     html += '</article></div>';
 
-  }
-  $('#container').append(html);
-};
+//   }
+//   $('#container').append(html);
+// };
 
-var showProject = function(){
-  console.log('showing all projects now');
-  $('.jumbotron').hide();
-  $('#container').empty();
+// var showProject = function(){
+//   console.log('showing all projects now');
+//   $('.jumbotron').hide();
+//   $('#container').empty();
 
-  $.ajax({
-    url: 'http://localhost:3000/projects', // add id for 'show' after this works
-    type: 'GET'
-  }).done(function(response){
-    renderProject(response.projects);
-  }).fail(function(jqXHR, textStatus, errorThrown){
-    trace(jqXHR, textStatus, errorThrown);
-  }).always(function(response){
-    trace(response);
-  });
-};
+//   $.ajax({
+//     url: 'http://localhost:3000/projects', // add id for 'show' after this works
+//     type: 'GET'
+//   }).done(function(response){
+//     renderProject(response.projects);
+//   }).fail(function(jqXHR, textStatus, errorThrown){
+//     trace(jqXHR, textStatus, errorThrown);
+//   }).always(function(response){
+//     trace(response);
+//   });
+// };
 
-$(document).ajaxStart(function(e){
-  trace(e, "starting an ajax request");
-  $('section#ajax-preloader').fadeIn();
-  $('section#container').fadeOut();
-});
+// $(document).ajaxStart(function(e){
+//   trace(e, "starting an ajax request");
+//   $('section#ajax-preloader').fadeIn();
+//   $('section#container').fadeOut();
+// });
 
-$(document).ajaxComplete(function(event, xhr, settings) {
-  /* executes whenever an AJAX request completes */
-  $('section#ajax-preloader').fadeOut();
-  $('section#container').fadeIn();
-});
+// $(document).ajaxComplete(function(event, xhr, settings) {
+//   /* executes whenever an AJAX request completes */
+//   $('section#ajax-preloader').fadeOut();
+//   $('section#container').fadeIn();
+// });
 
 // var projectRouter = new ProjectRouter();
 
 $(document).ready(function(){
   console.log('\'allo from the projects js!');
-  $('#projectlink').click(function() {
-    showProject();
-  });
 });
